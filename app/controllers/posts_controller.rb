4812d08c02a214b5ebe8c90e4882338b55958c2b
Rachel <rachel.kerner@gmail.com>
2017-07-17 08:48:43 -0500

    Commented out edit path, still getting errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,9 +20,9 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
-	def edit
-		@post = Post.find(params[:id])
-	end
+	# def edit
+	# 	@post = Post.find(params[:id])
+	# end
 
 	def update
 		@post = Post.find(params[:id])
